28111d260e0feba4a45b69e21ecef89696e5c714
Sokol1k <danil.sokolowskiy@gmail.com>
2020-03-17 23:50:05 +0200

    Created model, factory and seed for the products table

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,14 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\Product;
+use Faker\Generator as Faker;
+
+$factory->define(Product::class, function (Faker $faker) {
+    return [
+        'title' => $faker->text($maxNbChars = 50),
+        'description' => $faker->text($maxNbChars = 150),
+        'price' => 15,
+    ];
+});
